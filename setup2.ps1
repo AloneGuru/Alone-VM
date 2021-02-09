70f1e175d1c2f373853f0eaee18cbc12906d8ce4
Alone Guru <77436990+AloneGuru@users.noreply.github.com>
2021-02-10 00:48:12 +0530

    Update setup2.ps1

--- setup2.ps1
+++ setup2.ps1
@@ -1,5 +1,5 @@
-Invoke-WebRequest https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -OutFile C:\ngrok.zip
-Expand-Archive -Path C:\ngrok.zip -DestinationPath C:\ngrok
+Invoke-WebRequest https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -OutFile ngrok.zip
+Expand-Archive ngrok.zip
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 1
